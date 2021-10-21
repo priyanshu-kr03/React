23f789782bce0cc2a74e10159468971d1869183b
Priyanshu Kumar <priyanshu.kr81@gmail.com>
2021-10-21 14:46:59 +0530

    Module 5

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React , {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2020, 2, 0),
+  },
+  {
+    id: "e2",
+    title: "Toilet Paper",
+    amount: 50,
+    date: new Date(2022, 2, 3),
+  },
+  {
+    id: "e3",
+    title: "Grocessory",
+    amount: 500,
+    date: new Date(2021, 2, 3),
+  },
+  {
+    id: "e4",
+    title: "Tour",
+    amount: 1000,
+    date: new Date(2029, 2, 5),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2020, 2, 0),
-    },
-    {
-      id: "e2",
-      title: "Toilet Paper",
-      amount: 50,
-      date: new Date(2022, 2, 3),
-    },
-    {
-      id: "e3",
-      title: "Grocessory",
-      amount: 500,
-      date: new Date(2021, 2, 3),
-    },
-    {
-      id: "e4",
-      title: "Tour",
-      amount: 1000,
-      date: new Date(2029, 2, 5),
-    },
-  ];
+  const [expenses,setExpenses]=useState(DUMMY_EXPENSES);
+  
   const addExpenseHandler=expense=>{
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses=>{
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 const NewExpense=(props)=>{
+    const[isEditing,setIsEdditing]=useState(false);
     const saveExpenseDataHandler=(enteredExpenseData)=>{
         const expenseData={
             ...enteredExpenseData,
             id:Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEdditing(false);
+    };
+    const startEditingHandler=()=>{
+        setIsEdditing(true);
+    };
+    const stopEditingHandler=()=>{
+        setIsEdditing(false);
     };
     return(
         <div className="new-expense"> 
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing &&<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
         </div>
     )
 }
